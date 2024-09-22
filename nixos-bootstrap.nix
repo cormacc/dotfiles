74816f6ad3fdb4f33ec1d4b5c7554d1abec7ebe8
Cormac Cannon <cormacc@gmail.com>
2024-09-22 11:53:11 +0100

    new(nixos): Enable ssh service by default

--- nixos-bootstrap.nix
+++ nixos-bootstrap.nix
@@ -84,7 +84,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
--- nixos.nix
+++ nixos.nix
@@ -173,7 +173,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
