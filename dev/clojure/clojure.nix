de648a0c230b34c7deca27226826ba4d92a43217
Cormac Cannon <cormacc@gmail.com>
2025-09-04 18:12:51 +0100

    feat: Added polylith for clojure development

--- dev/clojure/clojure.nix
+++ dev/clojure/clojure.nix
@@ -3,8 +3,10 @@
 {
   home.packages = with pkgs; [
     # Compilers etc. for evaluating 3rd party repos without a flake...
+    jdk24
     # ... clojure
     clojure
+    polylith
     neil
     bbin
   ];
